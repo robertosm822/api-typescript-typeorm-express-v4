690276e7c7a55b94149de3eda027ed7f6de5f521
robertomelo822 <roberto@ezzepay.com.br>
2025-05-26 14:56:42 -0300

    Refactory updateRole

--- src/roles/useCases/updateRole/UpdateRoleUseCase.ts
+++ src/roles/useCases/updateRole/UpdateRoleUseCase.ts
@@ -1,6 +1,7 @@
 import { Role } from "@/roles/entities/Role";
-import { RolesRepository } from "@/roles/repositories/RolesRepository";
+import { IRolesRepository } from "@/roles/repositories/IRolesRepository";
 import { AppError } from "@/shared/errors/AppError";
+import { inject, injectable } from "tsyringe";
 
 
 type UpdateRoleDTO = {
@@ -8,8 +9,12 @@ type UpdateRoleDTO = {
   name: string;
 }
 
+@injectable()
 export class UpdateRoleUseCase {
-  constructor(private rolesRepository: RolesRepository) {}
+  constructor(
+    @inject('RolesRepository')
+    private rolesRepository: IRolesRepository
+  ) {}
 
   async execute({ id, name }: UpdateRoleDTO): Promise<Role> {
     const role = await this.rolesRepository.findById(id);
